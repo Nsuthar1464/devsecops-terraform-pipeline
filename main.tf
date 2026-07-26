b4f7f18fe9cd35a3a8a57bb35c49f0055de84c29
Nishant Suthar <suthar_nishant@yahoo.com>
2026-07-26 12:57:17 -0500

    Add intentionally insecure configuration

--- main.tf
+++ main.tf
@@ -5,3 +5,29 @@ provider "aws" {
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "lakeshore-devsecops-demo-bucket"
 }
+
+resource "aws_s3_bucket_public_access_block" "my_bucket_access" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_security_group" "bad_sg" {
+  name        = "open-to-world"
+  description = "Insecure security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
